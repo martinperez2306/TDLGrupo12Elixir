869b05b4187fbc178d37ba0f9148e26c27c3fcab
martinperez2306 <perezmartincontacto@gmail.com>
2019-10-19 16:53:13 -0300

    Agrego primeras pruebas de Test

---
+++ test.ex
@@ -0,0 +1,19 @@
+#Vamos a printear Hola Mundo!
+IO.puts "Hola Mundo!";
+IO.puts "Soy Tincho!";
+
+IO.puts :hola;
+
+saludo = "Hola gente!"
+IO.puts(saludo);
+
+x = 12;
+x = "Hello";
+IO.puts(x);
+
+
+list = [1,2,true,3];
+listASCII = [104,101,108, 108,111];
+IO.puts(listASCII);
+
+IO.puts(length([1, 2, :true, "str"]));
