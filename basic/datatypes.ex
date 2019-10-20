8e8b845d4177836a0c97e0584b11e6c4266790eb
martinperez2306 <perezmartincontacto@gmail.com>
2019-10-20 02:56:39 -0300

    Documentacion en codigo: PROCESOS!

---
+++ basic/datatypes.ex
@@ -0,0 +1,64 @@
+#Vamos a printear Hola Mundo!
+IO.puts "Hola Mundo!";
+IO.puts "Soy Tincho!";
+
+IO.puts :hola;
+
+saludo = "Hola gente!"
+IO.puts(saludo);
+
+x = 12;
+x = "Hello";
+IO.puts(x);
+
+
+list = [1,2,true,3];
+listASCII = [104,101,108, 108,111];
+IO.puts(listASCII);
+
+IO.puts(length([1, 2, :true, "str"]));
+
+#Esto es un ATOM
+#Su valor es su propio nombre
+:apple;
+#Usualmente para determinar estatus
+:ok;
+:error;
+
+#Booleanos son atoms y vicebersa
+result = :true == true;
+IO.puts(result);
+IO.puts(is_atom(false));
+IO.puts(is_boolean(:false));
+
+#Alias
+#Los alias son cosas que se pueden construir con mayus al inicio. Tambien son atomos.
+Hello
+IO.puts(is_atom(Hello))
+
+#Strings
+#Los strings son cadenas de caracteres y estan representados por el tipo BINARIO. Se usa con ""
+IO.puts(is_binary("Hello"))
+IO.puts(byte_size("Hello"))
+
+#Para concatenar strings o binarios se usa <>
+saludo = "Hello" <> "World"
+IO.puts(saludo)
+
+#Charlist. Son listas de caracteres. Se usa con ''
+to_charlist "hello"
+#'hello'
+to_string 'hello'
+#"hello"
+to_string :hello
+#"hello"
+to_string 1
+#"1"
+
+#Funciones anonimas
+add = fn a, b -> a + b end
+#Function<12.71889879/2 in :erl_eval.expr/5>
+add.(1, 2)
+#Result=3
+is_function(add)
+#Result=true
