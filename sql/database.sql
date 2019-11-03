7dce0ec25f28ba66586415a31dd109c87f56f6e7
martinperez2306 <perezmartincontacto@gmail.com>
2019-11-02 23:48:40 -0300

    Actualizo database

--- sql/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE chat_dev
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1;
-
-COMMENT ON DATABASE chat_dev
-    IS 'Base de datos de Desarrollo para TDL sobre el chat server';
