7e8af2049998e51d5620f4e7c3cc14ecf59eb775
martinperez2306 <perezmartincontacto@gmail.com>
2019-11-03 16:29:49 -0300

    GET y POST lobbies

--- chat_server_phoenix/lib/chat_web/controllers/lobby_controller.ex
+++ chat_server_phoenix/lib/chat_web/controllers/lobby_controller.ex
@@ -2,20 +2,14 @@ defmodule ChatWeb.LobbyController do
   use ChatWeb, :controller
 
   def index(conn, _params) do
-    lobbies = []
-    Chat.Repo.all(Chat.Lobby)
-    |> Enum.each(fn msg -> push(conn,"lobbies", %{
-        id: msg.id,
-        name: msg.name,
-        enable: msg.enable,
-      }) end)
-    #json conn, lobbies
+    lobbies = Chat.Repo.all(Chat.Lobby) |> Enum.map(&Map.take(&1,[:id, :name, :enable]))
+    json conn, lobbies
   end
 
   def create(conn, params) do
       changeset = Chat.Lobby.changeset(%Chat.Lobby{},params)
       case Chat.Repo.insert(changeset) do
-        {:ok, lobby} ->
+        {:ok, _lobby} ->
           json conn |> put_status(:created),%{ok: "Successful Operation"}
           {:error, _changeset} ->
             json conn |> put_status(:bad_request), %{errors: ["Unable to create lobby"] }
