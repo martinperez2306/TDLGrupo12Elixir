1d9903aa19e817b41de47bdd9e673b3691bbf8ed
aylenreynoso <aylu1996@gmail.com>
2019-11-06 18:57:48 -0300

    add get id

--- chat_server_phoenix/assets/js/app.js
+++ chat_server_phoenix/assets/js/app.js
@@ -12,19 +12,16 @@ import "phoenix_html"
 console.log("gesdd")       // name of message sender
 let btn = document.getElementById('loginBtn');
 
+function getId(response) {
 
-btn.addEventListener('click', function () {
-  
-  let pass = document.getElementById('pass');        // list of messages.
-  let email = document.getElementById('email');   
-  if (pass.value.length > 0 && email.value.length > 0) { // don't sent empty msg.
+    let id_json = JSON.parse(response);
+
     $.ajax({
-      'type': 'POST',
-      'url': 'http://localhost:4000/login',
+      'type': 'GET',
+      'url': 'http://localhost:4000/lobbies',
       'Content-Type': 'application/javascript',
-      'data': { 
-        "email" : email.value,
-        "pass" : pass.value
+      'data': {
+        "id" : id_json.id,
       },
       'success': function(response)
        {
@@ -32,9 +29,31 @@ btn.addEventListener('click', function () {
        },
        'error': function(jqXHR, textStatus, errorThrown)
        {
-           console.log('Error on saving appointment:', jqXHR, textStatus, errorThrown);    
+           console.log('Error on saving appointment:', jqXHR, textStatus, errorThrown);
+       }
+}
+
+
+btn.addEventListener('click', function () {
+
+  let pass = document.getElementById('pass');        // list of messages.
+  let email = document.getElementById('email');
+  if (pass.value.length > 0 && email.value.length > 0) { // don't sent empty msg.
+    $.ajax({
+      'type': 'POST',
+      'url': 'http://localhost:4000/login',
+      'Content-Type': 'application/javascript',
+      'data': {
+        "email" : email.value,
+        "pass" : pass.value
+      },
+      'success': function(response)
+       {
+        getId(response);
+       },
+       'error': function(jqXHR, textStatus, errorThrown)
+       {
+           console.log('Error on saving appointment:', jqXHR, textStatus, errorThrown);
        }
    });
-}});  
-
-
+}});
