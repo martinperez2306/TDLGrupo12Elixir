7f188c17e4527445b2f055d28cf139436a4747e2
Geronimo Illescas <ext_gillesca@mercadolibre.com>
2019-11-06 19:50:09 -0300

    Used beautify

--- chat_server_phoenix/lib/chat_web/router.ex
+++ chat_server_phoenix/lib/chat_web/router.ex
@@ -2,42 +2,41 @@ defmodule ChatWeb.Router do
   use ChatWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html http "]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html http "])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ChatWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", LoginController, :index
-
+    get("/", LoginController, :index)
   end
 
   scope "/login", ChatWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", LoginController, :index
-    post "/", LoginController, :login
-    put "/", LoginController, :create
+    get("/", LoginController, :index)
+    post("/", LoginController, :login)
+    put("/", LoginController, :create)
   end
 
   scope "/lobbies", ChatWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", LobbyController, :index
+    get("/", LobbyController, :index)
   end
 
   scope "/api", ChatWeb do
-    pipe_through :api
-    get "/lobbies", LobbyController, :index
-    post "/lobbies", LobbyController, :create
-    delete "/lobbies/:id", LobbyController, :delete
+    pipe_through(:api)
+    get("/lobbies", LobbyController, :index)
+    post("/lobbies", LobbyController, :create)
+    delete("/lobbies/:id", LobbyController, :delete)
   end
 
   # Other scopes may use custom stacks.
