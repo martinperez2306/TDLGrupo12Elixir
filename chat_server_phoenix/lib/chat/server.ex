1667d91c6f1b15350442aa9a5ec658382a30fcf1
Geronimo Illescas <ext_gillesca@mercadolibre.com>
2019-11-30 18:50:14 -0300

    Init new files

---
+++ chat_server_phoenix/lib/chat/server.ex
@@ -0,0 +1,43 @@
+defmodule Chat.Server do
+  use GenServer
+  # API
+  def start_link do
+    # We now start the GenServer with a `name` option.
+    GenServer.start_link(__MODULE__, [], name: :chat_room)
+  end
+
+  def start_link(name) do
+    # Instead of passing an atom to the `name` option, we send
+    # a tuple. Here we extract this tuple to a private method
+    # called `via_tuple` that can be reused in every function
+    GenServer.start_link(__MODULE__, [], name: via_tuple(name))
+  end
+
+  def add_message(room_name, message) do
+    # And the `GenServer` callbacks will accept this tuple the
+    # same way it accepts a pid or an atom.
+    GenServer.cast(via_tuple(room_name), {:add_message, message})
+  end
+
+  def get_messages(room_name) do
+    GenServer.call(via_tuple(room_name), :get_messages)
+  end
+
+  defp via_tuple(room_name) do
+    # And the tuple always follow the same format:
+    # {:via, module_name, term}
+    {:via, Chat.Registry, {:chat_room, room_name}}
+  end
+  # SERVER (no changes required here)
+
+  def init(messages) do
+    {:ok, messages}
+  end
+  def handle_cast({:add_message, new_message}, messages) do
+    {:noreply, [new_message | messages]}
+  end
+  def handle_call(:get_messages, _from, messages) do
+    {:reply, messages, messages}
+  end
+
+end
