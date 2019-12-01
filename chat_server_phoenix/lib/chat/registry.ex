18ad7be23ea86233fd13ae026c4001227291e140
Geronimo Illescas <ext_gillesca@mercadolibre.com>
2019-12-01 12:21:48 -0300

    Updated  actors files

--- chat_server_phoenix/lib/chat/registry.ex
+++ chat_server_phoenix/lib/chat/registry.ex
@@ -6,15 +6,19 @@ defmodule Chat.Registry do
     # just so we can reference it in the other functions.
     GenServer.start_link(__MODULE__, nil, name: :registry)
   end
+
   def whereis_name(room_name) do
     GenServer.call(:registry, {:whereis_name, room_name})
   end
+
   def register_name(room_name, pid) do
     GenServer.call(:registry, {:register_name, room_name, pid})
   end
+
   def unregister_name(room_name) do
     GenServer.cast(:registry, {:unregister_name, room_name})
   end
+
   def send(room_name, message) do
     # If we try to send a message to a process
     # that is not registered, we return a tuple in the format
@@ -24,6 +28,7 @@ defmodule Chat.Registry do
     case whereis_name(room_name) do
       :undefined ->
         {:badarg, {room_name, message}}
+
       pid ->
         Kernel.send(pid, message)
         pid
@@ -34,11 +39,13 @@ defmodule Chat.Registry do
   def init(_) do
     # We will use a simple Map to store our processes in
     # the format %{"room name" => pid}
-    {:ok, Map.new}
+    {:ok, Map.new()}
   end
+
   def handle_call({:whereis_name, room_name}, _from, state) do
     {:reply, Map.get(state, room_name, :undefined), state}
   end
+
   def handle_call({:register_name, room_name, pid}, _from, state) do
     # Registering a name is just a matter of putting it in our Map.
     # Our response tuple include a `:no` or `:yes` indicating if
@@ -46,10 +53,12 @@ defmodule Chat.Registry do
     case Map.get(state, room_name) do
       nil ->
         {:reply, :yes, Map.put(state, room_name, pid)}
+
       _ ->
         {:reply, :no, state}
     end
   end
+
   def handle_cast({:unregister_name, room_name}, state) do
     # And unregistering is as simple as deleting an entry
     # from our Map
--- chat_server_phoenix/lib/chat/server.ex
+++ chat_server_phoenix/lib/chat/server.ex
@@ -1,5 +1,6 @@
 defmodule Chat.Server do
   use GenServer
+
   # API
   def start_link do
     # We now start the GenServer with a `name` option.
@@ -13,31 +14,43 @@ defmodule Chat.Server do
     GenServer.start_link(__MODULE__, [], name: via_tuple(name))
   end
 
-  def add_message(room_name, message) do
+  def add_message(payload, lobby_id) do
     # And the `GenServer` callbacks will accept this tuple the
     # same way it accepts a pid or an atom.
-    GenServer.cast(via_tuple(room_name), {:add_message, message})
+    GenServer.cast(via_tuple(lobby_id), {:add_message, payload})
   end
 
-  def get_messages(room_name) do
-    GenServer.call(via_tuple(room_name), :get_messages)
+  def get_messages(room_id) do
+    GenServer.call(via_tuple(room_id), :get_messages)
   end
 
-  defp via_tuple(room_name) do
+  defp via_tuple(room_id) do
     # And the tuple always follow the same format:
     # {:via, module_name, term}
-    {:via, Chat.Registry, {:chat_room, room_name}}
+    {:via, Chat.Registry, {:chat_room, room_id}}
   end
-  # SERVER (no changes required here)
 
+  # SERVER
   def init(messages) do
     {:ok, messages}
   end
+
   def handle_cast({:add_message, new_message}, messages) do
-    {:noreply, [new_message | messages]}
-  end
-  def handle_call(:get_messages, _from, messages) do
-    {:reply, messages, messages}
+    # Insert in database
+    # {:ok, msg} = Chat.Message.changeset(%Chat.Message{}, new_message) |> Chat.Repo.insert()
+
+    # Cast te new_message received
+    message = for {key, val} <- new_message, into: %{}, do: {String.to_atom(key), val}
+
+    case messages do
+      [] -> {:noreply, [Map.put_new(message, :id, 0)]}
+      [head] -> {:noreply, [Map.put_new(message, :id, head.id + 1) | messages]}
+      [head | tail] -> {:noreply, [Map.put_new(message, :id, head.id + 1) | messages]}
+    end
   end
 
+  def handle_call(:get_messages, _from, messages) do
+    # Chat.Message.get_messages(Integer.to_string(messages.lobby_id))
+    {:reply, messages, messages}
+  end
 end
